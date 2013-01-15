ade3e3c02a8699c7274b2193c10a8cac72402b6f
YulongWu <wylong.0221@gmail.com>
2013-01-15 19:19:08 +0800

    First commit.

---
+++ SocialPie/src/onceportal/social/dataretriver/AuthorizerImpl.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package onceportal.social.dataretriver;
+
+import weibo4j.util.WeiboConfig;
+/**
+ * @author WU Yulong
+ *
+ */
+public class AuthorizerImpl implements IAuthorizer{
+
+	
+	public boolean authorize() {
+		
+	}
+	/**
+	 * 单元测试代码
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SocialPie/src/onceportal/social/sinaweibo/dataretriver/AuthorizerByOAuth2.java
@@ -0,0 +1,26 @@
+package onceportal.social.sinaweibo.dataretriver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import weibo4j.Oauth;
+import weibo4j.model.WeiboException;
+import weibo4j.util.BareBonesBrowserLaunch;
+/**
+ * 在新浪微博平台上通过OAuth2.0协议对用户进行验证
+ * @author WU Yulong
+ *
+ */
+public class AuthorizerByOAuth2 {
+
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
