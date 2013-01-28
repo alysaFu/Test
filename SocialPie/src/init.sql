ca507a8e76bcb6f9e70c8be1f1111337c600b536
WU Yulong <WU Yulong@WUYulong-LabPC>
2013-01-28 21:32:25 +0800

    补入了之前修改遗漏掉的tb_user表定义。

--- SocialPie/src/init.sql
+++ SocialPie/src/init.sql
@@ -1,7 +1,18 @@
 -- SocialPie的数据库初始化脚本
+create database if not exists SocialPie character set utf8;
 
 USE SocialPie;
-set names utf8;
+set names utf8; -- 控制台使用utf8编码
+-- 定义系统用户表
+drop table if exists tb_user;
+create table tb_user (
+	id integer auto_increment comment 'id',
+	name varchar(255) comment '用户名',
+	password varchar(255) comment '密码',
+	access_token varchar(255) comment '微博accessToken',
+	expire_date varchar(255) comment 'token过期时间',
+	primary key (id)
+);
 
 -- 定义微博用户表
 drop table if exists tb_s_user;
